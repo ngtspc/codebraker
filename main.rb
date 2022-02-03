91f2b11a30f2fa3b93ea2bf151bf89b074b95423
ngtspc <cube1090@gmail.com>
2022-02-03 16:24:06 +0200

    Changes in main class

--- main.rb
+++ main.rb
@@ -2,22 +2,21 @@ require_relative 'codebraker'
 require_relative 'cli'
 
 class Main
-  attr_accessor :current_attempts
+  attr_accessor :attempts_count
 
-  def initialize(current_attempts = 0)
+  def initialize(attempts_count = 0)
     @codebraker = Codebraker.new
-    @current_attempts = current_attempts
+    @attempts_count = attempts_count
   end
 
   def run
     CLI.greeting
-    @current_attempts = CLI.choose_attempts_number
-    (@current_attempts - 1).downto(0) do |current_attempt|
-      entered_number = Codebraker.enter_number
-      hidden_number = @codebraker.check(entered_number)
-      break if CLI.won_game?(hidden_number)
+    @attempts_count = CLI.choose_attempts_number
+    (@attempts_count - 1).downto(0) do |current_attempt|
+      hidden_number = @codebraker.check(Codebraker.enter_number)
+      return if CLI.won_game?(hidden_number)
 
-      CLI.decrease_attempts_counter(current_attempt)
+      CLI.show_attempts(current_attempt)
     end
     CLI.game_over?
   end
